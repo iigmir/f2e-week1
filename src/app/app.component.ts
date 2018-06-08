94c06684a6a09e788ea35774223cef42123d30e7
iigmir <roc120j@gmail.com>
2018-06-08 17:19:05 +0800

    Import service, and adjust styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { TodoService } from './todo.service';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'app';
+    title = 'app';
+    todos: Todo[];
+    constructor( private serv: TodoService ) {}
+    ngInit() {}
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../todo.service';
+import { Todo } from '../todo';
+
 
 @Component({
-  selector: 'app-tasks',
-  templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.scss']
+    selector: 'app-tasks',
+    templateUrl: './tasks.component.html',
+    styleUrls: ['./tasks.component.scss']
 })
+
 export class TasksComponent implements OnInit {
-
-  constructor(  ) { }
-
-  ngOnInit() {
-  }
-
+    todos: Todo[];
+    constructor( private service: TodoService ) { }
+    ngOnInit() {
+        this.todos = this.service.getTodos();
+        console.log( this.todos );
+    }
+    logIt() {
+        console.log('Hello');
+    }
 }
